bc0242fb621d3162a9e808294e27fc516f40e24d
Jeff Johnson <jeff@gridfinity.com>
2020-11-03 03:18:13 -0500

    blockchain: Avoid deadlock with additional locking

--- blockchain/chain.go
+++ blockchain/chain.go
@@ -156,8 +156,9 @@ type BlockChain struct {
 	//
 	// In addition, some of the fields are stored in the database so the
 	// chain state can be quickly reconstructed on load.
-	stateLock     sync.RWMutex
-	stateSnapshot *BestState
+	stateLock        sync.RWMutex
+	stateSnapshot    *BestState
+	notificationLock sync.Mutex
 
 	// The following caches are used to efficiently keep track of the
 	// current deployment threshold state of each rule change deployment.
@@ -685,9 +686,11 @@ func (b *BlockChain) connectBlock(node *blockNode, block *btcutil.Block,
 	// Notify the caller that the block was connected to the main chain.
 	// The caller would typically want to react with actions such as
 	// updating wallets.
+	b.notificationLock.Lock()
 	b.chainLock.Unlock()
 	b.sendNotification(NTBlockConnected, block)
 	b.chainLock.Lock()
+	b.notificationLock.Unlock()
 
 	return nil
 }
@@ -810,9 +813,13 @@ func (b *BlockChain) disconnectBlock(node *blockNode, block *btcutil.Block, view
 	// Notify the caller that the block was disconnected from the main
 	// chain.  The caller would typically want to react with actions such as
 	// updating wallets.
+	b.notificationLock.Lock()
 	b.chainLock.Unlock()
 	b.sendNotification(NTBlockDisconnected, block)
 	b.chainLock.Lock()
+	b.notificationLock.Unlock()
+	b.stateLock.Lock()
+	defer b.stateLock.Unlock()
 
 	return nil
 }
