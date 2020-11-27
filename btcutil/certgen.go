243e9bdea74bed96e88ad5b178789c43059c3056
Jeff Johnson <jeff@gridfinity.com>
2020-11-26 22:35:07 -0500

    btcutil/certgen: Simplify expression > method call

--- btcutil/certgen.go
+++ btcutil/certgen.go
@@ -61,7 +61,7 @@ func NewTLSCertPair(organization string, validUntil time.Time, extraHosts []stri
 
 	addIP := func(ipAddr net.IP) {
 		for _, ip := range ipAddresses {
-			if net.IP.Equal(ip, ipAddr) {
+			if ip.Equal(ipAddr) {
 				return
 			}
 		}
