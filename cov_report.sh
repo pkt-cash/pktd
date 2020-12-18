bac5821560194efc597d5fd7c4b497e71b348042
Jeff Johnson <jeff@gridfinity.com>
2020-12-18 08:13:45 -0500

    coverage: Correct reversed test conditions

--- cov_report.sh
+++ cov_report.sh
@@ -6,9 +6,9 @@ global_trap() {	err="${?}"; trap - EXIT; trap '' EXIT INT TERM QUIT HUP; cleanUp
 trap 'global_trap $?' EXIT; trap 'err=$?; global_trap; exit $?' HUP TERM;
 trap 'err=$?; trap - EXIT global_trap $err; exit $err' QUIT;
 trap 'global_trap; trap - INT; kill -INT $$; sleep 1; trap - TERM; kill -TERM $$' INT; # shellcheck disable=SC2154,SC2236
-if [ ! -z "${TEST_LND}" ]; then LNDTEST='lnd' && export LNDTEST && LGTTEST='lightning' && export LGTTEST;
+if [ -z "${TEST_LND}" ]; then LNDTEST='lnd' && export LNDTEST && LGTTEST='lightning' && export LGTTEST;
 	else LNDTEST="_X_X_X_X_" && export LNDTEST && LGTTEST="_X_X_X_X_" && export LGTTEST; fi; # shellcheck disable=SC2154,SC2236
-if [ ! -z "${TEST_LDB}" ]; then LDBTEST='goleveldb' && export LDBTEST;
+if [ -z "${TEST_LDB}" ]; then LDBTEST='goleveldb' && export LDBTEST;
 	else LDBTEST="_X_X_X_X_" && export LDBTEST; fi;
 if [ ! -f "./.pktd_root" ]; then printf '%s\n' "Error: Must execute in the pktd source directory."; exit 1; fi;
 CGO_ENABLED=0 && export CGO_ENABLED; # shellcheck disable=SC2089,SC2090
